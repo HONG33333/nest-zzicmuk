f6993465180020642c381fe2de4f98dbd953e33e
sam <j2d1005@naver.com>
2023-12-26 22:54:35 +0900

    Update Boards Service > getAllBoards

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  private boards = [1, 2, 3, 4, 5];
+
+  getAllBoards() {
+    return this.boards;
+  }
+}
