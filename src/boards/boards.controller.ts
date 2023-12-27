9454fde39af91a4e48029e5cc0aa9362cf6de2b5
sam <j2d1005@naver.com>
2023-12-27 10:05:18 +0900

    Update Boards Service > getBoardById, controller에서 param받기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dtd/create-board.dto';
 
@@ -9,6 +9,15 @@ export class BoardsController {
   @Get('/')
   getAllBoards() {
     return this.boardsService.getAllBoards();
+  }
+
+  @Get('/:id')
+  // @Get('/:id/:name')
+  // param 여러개일 때는 getBoardById(@Param('id') id: string, @Param('name') name: string) {}
+  // param 여러개일 때는 getBoardById(@Param() params: string[]) {}
+  // DTO 만드는기준???
+  getBoardById(@Param('id') id: string) {
+    return this.boardsService.getBoardById(id);
   }
 
   @Post()
