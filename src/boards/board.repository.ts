b4864c5d1103bca32f944a527f75b1f1c686bb83
sam <j2d1005@naver.com>
2023-12-28 23:31:59 +0900

    Fix 오타 수정

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -1,5 +1,5 @@
 import { Repository } from 'typeorm';
-import { Board } from './board.entitiy';
+import { Board } from './board.entity';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
