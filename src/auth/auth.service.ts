6b5ff5861a391c458bc5428d28b1e717b1430019
sam <j2d1005@naver.com>
2024-01-01 23:01:06 +0900

    Add Auth > signIn

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
   constructor(private authRepository: UserRepository) {}
+
+  signUp(authCredentialsDto: AuthCredentialsDto) {
+    return this.authRepository.createUser(authCredentialsDto);
+  }
 }
