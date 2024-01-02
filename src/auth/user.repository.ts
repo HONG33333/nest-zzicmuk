4828f8646c3a9a43afd83461ea8e6878bbf60640
sam <j2d1005@naver.com>
2024-01-02 22:50:47 +0900

    Update Auth > signUp username Unique throw error

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,10 @@
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
@@ -13,6 +17,15 @@ export class UserRepository extends Repository<User> {
     const { username, password } = authCredentialsDto;
     const user = this.create({ username, password });
 
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        // Unique 값이 아닌 경우
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
