3226d286b810dab04ddc00f37575ce21d09af62d
sam <j2d1005@naver.com>
2024-01-02 23:34:35 +0900

    Update Auth > signIn

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
-  constructor(private authRepository: UserRepository) {}
+  constructor(private userRepository: UserRepository) {}
 
   signUp(authCredentialsDto: AuthCredentialsDto) {
-    return this.authRepository.createUser(authCredentialsDto);
+    return this.userRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto) {
+    const { username, password } = authCredentialsDto;
+
+    const user = await this.userRepository.findOneBy({ username });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'login success';
+    } else {
+      throw new UnauthorizedException('login failed');
+    }
   }
 }
