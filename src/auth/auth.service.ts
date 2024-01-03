a5d3644de58d4ccbe4a735cedc357e06ea0c009e
sam <j2d1005@naver.com>
2024-01-03 11:39:04 +0900

    Update Auth > signIn 토큰 발급

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,14 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userRepository: UserRepository) {}
+  constructor(
+    private userRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
 
   signUp(authCredentialsDto: AuthCredentialsDto) {
     return this.userRepository.createUser(authCredentialsDto);
@@ -17,7 +21,11 @@ export class AuthService {
     const user = await this.userRepository.findOneBy({ username });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'login success';
+      // 유저 토큰 생성 (Secret + payload 필요)
+      const payload = { username }; // 중요 정보 포함하면 안된다
+      const accessToken = this.jwtService.sign(payload);
+
+      return { accessToken };
     } else {
       throw new UnauthorizedException('login failed');
     }
