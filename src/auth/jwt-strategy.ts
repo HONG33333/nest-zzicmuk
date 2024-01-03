4ca3ba325a020f9b292105dabc4ac9b6903590b6
sam <j2d1005@naver.com>
2024-01-03 12:13:28 +0900

    Update Auth > JwtStrategy 생성, 모듈 등록, 내보내기

---
+++ src/auth/jwt-strategy.ts
@@ -0,0 +1,28 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserRepository } from './user.repository';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private userRepository: UserRepository) {
+    super({
+      // jwt module register 에서 한 설정과 동일한 secretKey
+      secretOrKey: 'secret12333',
+      // 헤더의 베어러 토큰에서 추출하겠다 라는 뜻
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: AuthCredentialsDto) {
+    const { username } = payload;
+    const user = await this.userRepository.findOneBy({ username });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
